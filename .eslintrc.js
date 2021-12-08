fde616988dd98cdf7ba7143cabc10107679a8e9e
marian0 <marianosantafe@gmail.com>
2021-12-08 16:37:01 -0300

    unified prettierc settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   root: true,
   env: {
     node: true,
@@ -17,5 +20,32 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        arrowParens: "always",
+        bracketSpacing: true,
+        embeddedLanguageFormatting: "auto",
+        insertPragma: false,
+        printWidth: 100,
+        proseWrap: "preserve",
+        quoteProps: "as-needed",
+        requirePragma: false,
+        semi: true,
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: "all",
+        useTabs: false,
+      },
+    ],
   },
 };
